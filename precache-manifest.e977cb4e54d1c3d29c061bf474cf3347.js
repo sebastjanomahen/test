1a3353fbe6f6867fed5f8bc7610dd30ee4c3d4d0
Sebastjan O <sebaomahen@gmail.com>
2020-05-07 23:28:18 +0200

    Updates

---
+++ precache-manifest.e977cb4e54d1c3d29c061bf474cf3347.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465ee199271306a7216552ab9b77eb2d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "30aa4e1a0086057dc75d",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/test/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30aa4e1a0086057dc75d",
+    "url": "/test/static/js/main.1b158f67.chunk.js"
+  },
+  {
+    "revision": "9b5f83bf8bbf286a60f4",
+    "url": "/test/static/js/runtime-main.9b619a2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
